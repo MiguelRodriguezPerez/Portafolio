c1adcb6701a2df76e6dec7be8b0b1bb432cc8048
Miguel Rodríguez <jazmindemar33@gmail.com>
2026-02-04 19:19:16 +0100

    css env + hoverup socials

--- src/components/aboutMe/aboutMeInfo/aboutMeContact/ContactItem.tsx
+++ src/components/aboutMe/aboutMeInfo/aboutMeContact/ContactItem.tsx
@@ -4,9 +4,10 @@ import type { contactInterface } from "../../../../interfaces";
 import styles from '../../../../styles/aboutMe/contact.module.css';
 import svgStyle from '../../../../styles/shared/svgDiv.module.css';
 
+
 export const ContactItem = ({ contactData }: { contactData: contactInterface }) => {
 
-    const Icon = contactData.icon
+    const Icon = contactData.icon;
 
     const clickEvent = () => {
         window.open(contactData.link);
